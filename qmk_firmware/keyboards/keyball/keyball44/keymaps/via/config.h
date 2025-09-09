15a65412919a07abecddcf9adf1b41d684f31f13
HAYASHI Naoya <fuzakebito@fuzake.io>
2025-09-09 18:46:54 +0900

    軽量化, レイヤー増加, kvm対応, PERMISSIVE_HOLD, EXTRAKEY_ENABLE

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -37,3 +37,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
+
+#define LAYER_STATE_8BIT
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+#define PERMISSIVE_HOLD
+
+#define OS_DETECTION_KEYBOARD_RESET
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_ACTION_ONESHOT
+#define NO_MUSIC_MODE
